7824719150f21091bad0b44ef1786d17e6ff6f11
Michael Watson <michael@dcpolicycenter.org>
2018-03-29 13:44:48 -0400

    trying to add navbar files

---
+++ assets/js/navbar2.js
@@ -0,0 +1,10 @@
+window.onscroll = function() {myFunction()};
+var navbar = document.getElementById("navbar-2");
+var sticky = navbar.offsetTop;
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
