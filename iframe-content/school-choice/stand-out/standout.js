bda4c2d0bce2ce2b9792facd3d4d1c4083b9451b
sr-dc-policy-center <31214373+sr-dc-policy-center@users.noreply.github.com>
2018-04-06 10:54:58 -0400

    Add files via upload

---
+++ iframe-content/school-choice/stand-out/standout.js
@@ -0,0 +1,36 @@
+function myFunctionPart() {
+  var x = document.getElementById("myDIV");
+  if (x.innerHTML != "Highest and Lowest In-Boundary Participation Rates") {
+    x.innerHTML = "<h3>Highest and Lowest In-Boundary Participation Rates</h3>";
+  } else {
+    x.innerHTML = " ";
+  }
+}
+
+function myFunctionDraw() {
+  var x = document.getElementById("myDIV");
+  if (x.innerHTML != "Schools that Draw More or Fewer Students than Expected") {
+    x.innerHTML = "<h3>Schools that Draw More or Fewer Students than Expected</h3>";
+  } else {
+    x.innerHTML = " ";
+  }
+}
+
+function myFunctionAa() {
+  var x = document.getElementById("myDIV");
+  if (x.innerHTML != "Schools that are More or Less African-American than the Neighborhood") {
+    x.innerHTML = "<h3>Schools that are More or Less African-American than the Neighborhood</h3>";
+  } else {
+    x.innerHTML = " ";
+  }
+}
+
+function toggleFocus(el)
+{
+ el.style.backgroundColor =  el.style.backgroundColor=="e2e2e2" ? "inherit" : "e2e2e2";
+}
+
+function toggleOffFocus(el)
+{
+ el.style.backgroundColor =  el.style.backgroundColor=="#fbfbfb" ? "inherit" : "#fbfbfb";
+}
